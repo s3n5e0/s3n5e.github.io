9b2225f9f484a45394653151f0aeef4b4c546bf1
s3n5e <93555350+s3n5e0@users.noreply.github.com>
2022-10-29 10:51:42 +0800

    Delete WWW directory

--- WWW/json.php
+++
@@ -1,6 +0,0 @@
-<?php
-$ip = isset($_GET['ip']) ? $_GET['ip'] : 0;
-$ip = json_decode($ip,true);
-$ip = $ip['query'];
-echo ($ip);
-?>
--- WWW/shell/shell.bat
+++
@@ -1,2 +0,0 @@
-echo for /f "delims=" %t in ('cls&whoami>tmp') do echo %t>192.168.1.10
-pause
